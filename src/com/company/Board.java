81931f61daf7b0b9a489bf50a032487135737922
sswetonic <stephen.swetonic@go.shoreline.edu>
2019-01-13 19:56:36 -0800

    test

---
+++ src/com/company/Board.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+public class Board {
+
+    private ArrayList<ArrayList<Character>> ar = new ArrayList<ArrayList<Character>>();
+
+    public Board(File f) throws FileNotFoundException{
+        Scanner sc = new Scanner(f);
+        while(sc.hasNextLine()) {
+            ArrayList<Character> row = new ArrayList<Character>();
+            String line1 = sc.nextLine();
+            for(int i = 0; i < line1.length(); i++) {
+                row.add(line1.charAt(i));
+            }
+            ar.add(row);
+        }
+    }
+
+    public String toString() {
+        String toReturn = "";
+        for(ArrayList<Character> val : ar) {
+            for(int i = 0; i < val.size(); i++) {
+                toReturn += val.get(i) + " ";
+            }
+            toReturn += "\n";
+        }
+        return toReturn;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
+import java.io.*;
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException{
+
+        File boardFile = new File("src/board.sdk");
+
+        Board b = new Board(boardFile);
+        System.out.println(b);
+
     }
 }
